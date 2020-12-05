afef10787e86cb5e4fad23a04e5554d7b891b236
Neil Sebbey <neilsebbey@outlook.com>
2020-12-05 10:17:32 +0000

    Added Customers.java

---
+++ src/mobilePlans/Customers.java
@@ -0,0 +1,11 @@
+package mobilePlans;
+
+public class Customers {
+    private int custId;
+    private String Surname;
+    private String Forename;
+    private String Email;
+    private String Address;
+    private String City;
+    private String planId;
+}
