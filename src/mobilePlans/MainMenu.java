71ff45fde2f2fbe9567d760add64ca61edc23bc4
Neil Sebbey <neilsebbey@outlook.com>
2020-12-05 10:17:00 +0000

    Added icon.png

--- src/mobilePlans/MainMenu.java
+++ src/mobilePlans/MainMenu.java
@@ -2,6 +2,10 @@ package mobilePlans;
 /* Neil Sebbey (T00198203) - Software Development Stream (Yr. 2)
    OOP CA 2 - due for Sat, 5th Dec 2020 */
 
-public class MainMenu {
+import javax.swing.*;
+import java.awt.event.ActionListener;
 
+public class MainMenu extends JFrame implements ActionListener {
+
+    private JMenu plansMenu, customersMenu,
 }
--- src/mobilePlans/Plans.java
+++ src/mobilePlans/Plans.java
@@ -1,6 +1,9 @@
-package mobilePlans;//mobilePlans.Plans.java
+package mobilePlans;
+//Plans.java
 /* Neil Sebbey (T00198203) - Software Development Stream (Yr. 2)
-   OOP CA 2 - due for Sat, 5th Dec 2020 */
+ * OOP CA 2 - due for Sat, 5th Dec 2020
+ * @author  Neil Sebbey
+ * @since   14-11-2020  */
 
 public class Plans {
     private int planId;
